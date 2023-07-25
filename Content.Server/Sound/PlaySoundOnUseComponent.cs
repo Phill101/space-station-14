f9d4a96d3a14f92d8592141487fe10e338013e93
Phill101 <holypics4@gmail.com>
2023-07-25 13:44:21 +0200

    my horn

---
+++ Content.Server/Sound/PlaySoundOnUseComponent.cs
@@ -0,0 +1,8 @@
+﻿namespace Content.Server.Sound;
+
+[RegisterComponent]
+public class PlaySoundOnUseComponent : Component
+{
+    [DataField("sound")]
+    public string Sound = string.Empty;
+}
---
+++ Content.Server/Sound/PlaySoundOnUseSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Server.Cooldown;
+using Content.Shared.Cooldown;
+using Content.Shared.Interaction.Events;
+using Robust.Shared.Timing;
+
+namespace Content.Server.Sound;
+
+public class PlaySoundOnUseSystem : EntitySystem
+{
+
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<PlaySoundOnUseComponent, UseInHandEvent>(OnUseInHand);
+    }
+
+    private void OnUseInHand(EntityUid uid, PlaySoundOnUseComponent component, UseInHandEvent args)
+    {
+        var curTime = _gameTiming.CurTime;
+        if (TryComp<ItemCooldownComponent>(uid, out var cooldown) && curTime < cooldown.CooldownEnd)
+        {
+            return;
+        }
+
+        _audio.PlayPvs(component.Sound, uid);
+
+        RaiseLocalEvent(
+            uid,
+            new RefreshItemCooldownEvent(curTime, curTime + TimeSpan.FromSeconds(3))
+        );
+    }
+}
